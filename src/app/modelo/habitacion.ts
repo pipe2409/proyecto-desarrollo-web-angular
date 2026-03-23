eb8d996db3c24246d0e30891d37d10b0cbe66d7f
pipe2409 <andresbe1205@gmail.com>
2026-03-23 12:52:59 -0500

    Clase habitacion terminada

--- src/app/modelo/habitacion.ts
+++ src/app/modelo/habitacion.ts
@@ -1,2 +1,28 @@
+import {TipoHabitacion} from './tipo-habitacion';
+
 export class Habitacion {
+
+  id?: number;
+  codigo?: string;
+  piso?: number;
+  estado?: string;
+  tipoHabitacion?: TipoHabitacion;
+  notas?: string;
+
+  constructor(
+    id?: number,
+    codigo?: string,
+    piso?: number,
+    estado?: string,
+    tipoHabitacion?: TipoHabitacion,
+    notas?: string
+  ) {
+    this.id = id;
+    this.codigo = codigo;
+    this.piso = piso;
+    this.estado = estado;
+    this.tipoHabitacion = tipoHabitacion;
+    this.notas = notas;
+  }
+
 }
