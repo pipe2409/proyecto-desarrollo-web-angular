cb8396efc3e16cabb0e965a131e2bdd24ce66d78
pipe2409 <andresbe1205@gmail.com>
2026-03-23 13:09:55 -0500

    Clase reserva terminada

--- src/app/modelo/reserva.ts
+++ src/app/modelo/reserva.ts
@@ -1,2 +1,37 @@
+import { CuentaHabitacion } from './cuenta-habitacion';
+import { Huesped } from './huesped';
 export class Reserva {
+
+  id?: number;
+  fechaInicio?: Date;
+  fechaFin?: Date;
+  cantidadPersonas?: number;
+  estado?: string;
+  huesped?: Huesped;
+  habitacion?: Habitacion;
+  operador?: Operador;
+  cuentaHabitacion?: CuentaHabitacion;
+
+  constructor(
+    id?: number,
+    fechaInicio?: Date,
+    fechaFin?: Date,
+    cantidadPersonas?: number,
+    estado?: string,
+    huesped?: Huesped,
+    habitacion?: Habitacion,
+    operador?: Operador,
+    cuentaHabitacion?: CuentaHabitacion
+  ) {
+    this.id = id;
+    this.fechaInicio = fechaInicio;
+    this.fechaFin = fechaFin;
+    this.cantidadPersonas = cantidadPersonas;
+    this.estado = estado;
+    this.huesped = huesped;
+    this.habitacion = habitacion;
+    this.operador = operador;
+    this.cuentaHabitacion = cuentaHabitacion;
+  }
+
 }
