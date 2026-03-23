f937b44614bad04eae0338ba0b8502313d4e3477
pipe2409 <andresbe1205@gmail.com>
2026-03-23 13:18:24 -0500

    Clase itemcuenta terminado

--- src/app/modelo/item-cuenta.ts
+++ src/app/modelo/item-cuenta.ts
@@ -1,2 +1,26 @@
-export class Itemcuenta {
+import { CuentaHabitacion } from './cuenta-habitacion';
+import { Servicio } from './servicio';
+
+export class ItemCuenta {
+
+  id?: number;
+  cantidad?: number;
+  subtotal?: number;
+  cuentaHabitacion?: CuentaHabitacion;
+  servicio?: Servicio;
+
+  constructor(
+    id?: number,
+    cantidad?: number,
+    subtotal?: number,
+    cuentaHabitacion?: CuentaHabitacion,
+    servicio?: Servicio
+  ) {
+    this.id = id;
+    this.cantidad = cantidad;
+    this.subtotal = subtotal;
+    this.cuentaHabitacion = cuentaHabitacion;
+    this.servicio = servicio;
+  }
+
 }
