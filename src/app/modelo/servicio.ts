de0e0a43702b718dcfd916a7ce7fb3562c884ae4
pipe2409 <andresbe1205@gmail.com>
2026-03-23 13:02:14 -0500

    Clase servicio terminada

--- src/app/modelo/servicio.ts
+++ src/app/modelo/servicio.ts
@@ -1,2 +1,32 @@
 export class Servicio {
+
+  id?: number;
+  nombre?: string;
+  descripcion?: string;
+  precio?: number;
+  imagenUrl?: string;
+  capacidad?: number;
+  precioTipo?: string;
+  horario?: string;
+
+  constructor(
+    id?: number,
+    nombre?: string,
+    descripcion?: string,
+    precio?: number,
+    imagenUrl?: string,
+    capacidad?: number,
+    precioTipo?: string,
+    horario?: string
+  ) {
+    this.id = id;
+    this.nombre = nombre;
+    this.descripcion = descripcion;
+    this.precio = precio;
+    this.imagenUrl = imagenUrl;
+    this.capacidad = capacidad;
+    this.precioTipo = precioTipo;
+    this.horario = horario;
+  }
+
 }
