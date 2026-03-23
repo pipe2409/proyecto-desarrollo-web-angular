7dcf63f65678c0897ca573935fa86e18b84e71bc
pipe2409 <andresbe1205@gmail.com>
2026-03-23 12:56:27 -0500

    Clase tipohabitacion terminada

--- src/app/modelo/tipo-habitacion.ts
+++ src/app/modelo/tipo-habitacion.ts
@@ -1,3 +1,35 @@
 export class TipoHabitacion {
-    
+
+  id?: number;
+  nombre?: string;
+  descripcion?: string;
+  precio?: number;
+  imagenUrl?: string;
+  capacidad?: number;
+  camas?: string;
+  amenities?: string;
+  disponible?: boolean;
+
+  constructor(
+    id?: number,
+    nombre?: string,
+    descripcion?: string,
+    precio?: number,
+    imagenUrl?: string,
+    capacidad?: number,
+    camas?: string,
+    amenities?: string,
+    disponible?: boolean
+  ) {
+    this.id = id;
+    this.nombre = nombre;
+    this.descripcion = descripcion;
+    this.precio = precio;
+    this.imagenUrl = imagenUrl;
+    this.capacidad = capacidad;
+    this.camas = camas;
+    this.amenities = amenities;
+    this.disponible = disponible;
+  }
+
 }
