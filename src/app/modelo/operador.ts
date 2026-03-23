9c66525a5537836839d62da9be2dfb21877d9166
pipe2409 <andresbe1205@gmail.com>
2026-03-23 13:00:20 -0500

    Clase operador terminada

--- src/app/modelo/operador.ts
+++ src/app/modelo/operador.ts
@@ -1,2 +1,17 @@
 export class Operador {
+
+  id?: number;
+  correo?: string;
+  contrasena?: string;
+
+  constructor(
+    id?: number,
+    correo?: string,
+    contrasena?: string
+  ) {
+    this.id = id;
+    this.correo = correo;
+    this.contrasena = contrasena;
+  }
+
 }
