9cb0434d11bae55d515a38648bd55c5c331a12cf
pipe2409 <andresbe1205@gmail.com>
2026-04-12 20:32:26 -0500

    Servicio servicio con rxjs

--- src/app/services/servicios.service.ts
+++ src/app/services/servicios.service.ts
@@ -1,19 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { Servicio } from '../modelo/servicio';
-import { SERVICES_DATA } from '../features/landing/data/servicio.data';
+import { environment } from '../../environments/environment';
+//import { SERVICES_DATA } from '../features/landing/data/servicio.data';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiciosService {
 
-  private servicios = SERVICES_DATA;
+//private servicios = SERVICES_DATA;
 
-  constructor() {}
+private apiUrl = `${environment.apiUrl}/api/servicios`; // 
+constructor(private http: HttpClient) {}
 
+getAll(): Observable<Servicio[]> {
+    return this.http.get<Servicio[]>(this.apiUrl);
+  }
+
+  //Codigo servicio usando datos locales
+  /*constructor() {}
+
+   
   getAll(): Servicio[] {
     return this.servicios;
   }
+
+  
+
 
   getById(id: number): Servicio | undefined {
     return this.servicios.find(s => s.id === id);
@@ -36,4 +51,5 @@ export class ServiciosService {
       this.servicios.splice(index, 1);
     }
   }
+  */
 }
